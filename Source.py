c4f05d22d5fb4f2ce925b9db067c56985e633151
Nathan Holdom <nathanholdom1@gmail.com>
2020-05-05 18:15:46 -0400

    Cleared database for real testing

--- Source.py
+++ Source.py
@@ -2,15 +2,13 @@ from IngredientManager import IngredientManager
 import sqlite3
 
 if __name__ == "__main__":
-    Manager = IngredientManager()
-    print(Manager.db_contents())
-
-    # # Getting ingredients
-    # Manager.get_ingredients()
-    # print(Manager.ingredients)
-    # # Pushing ingredients to database
-    # Manager.commit()
+    # conn = sqlite3.connect("Kitchenet.db")
     #
-    # print(Manager.db_contents())
+    # conn.execute("DELETE FROM Fridge")
+    #
+    # conn.commit()
+    # conn.close()
 
-    Manager.close()
+    manager = IngredientManager()
+    print(manager.db_contents())
+    manager.close()
